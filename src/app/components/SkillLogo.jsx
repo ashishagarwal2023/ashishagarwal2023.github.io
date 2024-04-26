805593fe9dad711494a17c094797c1f6eb8338ad
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-26 20:07:32 +0530

    mhm

--- src/app/components/SkillLogo.jsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export const SkillLogo = ({path, language}) => {
-	return(
-		<Image src={`/images/icons/${path}`} className="max-h-8" width={32} height={32} alt={language} />
-	)
-}
